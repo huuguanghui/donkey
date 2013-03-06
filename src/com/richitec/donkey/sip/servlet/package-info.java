6034be76a57a68c33b7ea6f644bf52088504da35
huuguanghui <huuguanghui@hotmail.com>
2013-03-06 10:54:48 +0800

    change SipApplicationSession timeout=30 minutes

--- src/com/richitec/donkey/sip/servlet/package-info.java
+++ src/com/richitec/donkey/sip/servlet/package-info.java
@@ -1,4 +1,4 @@
 
 @javax.servlet.sip.annotation.SipApplication(name="donkey", 
-		mainServlet="mainServlet", sessionTimeout=10)
+		mainServlet="mainServlet", sessionTimeout=30)
 package com.richitec.donkey.sip.servlet;
